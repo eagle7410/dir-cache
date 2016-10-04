0fa2f0b1e3971a45bdb9498c1f0144ca6f96d2c4
Igor Stcherbina <orel1939@mail.ru>
2016-10-04 19:27:06 +0300

    Add cache component.

--- index.js
+++ index.js
@@ -2,23 +2,32 @@
  * Created by igor on 03.10.16.
  */
 var cacheDir = require('./lib/dir-cache');
-var scan = cacheDir.required({dirs: 'cabmin/controllers' }, (e, scan)  => {
+var cache = cacheDir.cache().data ={
+	p1 : {p11 : 12},
+	p2 : 2,
+	p3 : 3
+};
 
-	if (e) {
-		return console.log('ERR : ', e );
-	}
-
-	scan.run((e, scan) => {
-		if (e) {
-			return console.log('ERR : ', e );
-		}
-
-		var ut = require('util');
-		console.log('scan ', ut.inspect(scan, false,  null));
-	});
-
-
-});
+// TODO: clear
+console.log('cache ', cache);
+console.log('cache ', cache.p1);
+//var scan = cacheDir.required({dirs: 'cabmin/controllers' }, (e, scan)  => {
+//
+//	if (e) {
+//		return console.log('ERR : ', e );
+//	}
+//
+//	scan.run((e, scan) => {
+//		if (e) {
+//			return console.log('ERR : ', e );
+//		}
+//
+//		var ut = require('util');
+//		console.log('scan ', ut.inspect(scan, false,  null));
+//	});
+//
+//
+//});
 //var scan = cacheDir.read({dirs: 'cabmin/views', exts : ['.html']}, (e, scan)  => {
 //
 //	if (e) {
