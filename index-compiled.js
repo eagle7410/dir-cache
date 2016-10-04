18d2f7f08fd245f2228b69671384685dfe5f0a25
Igor Stcherbina <orel1939@mail.ru>
2016-10-04 13:05:46 +0300

    Swig chager ready.

---
+++ index-compiled.js
@@ -0,0 +1,22 @@
+/**
+ * Created by igor on 03.10.16.
+ */
+var cacheDir = require('./lib/dir-cache');
+
+var scan = cacheDir.read({ dirs: 'cabmin/views', exts: ['.html'] }, (e, scan) => {
+
+	if (e) {
+		return console.log('ERR : ', e);
+	}
+
+	scan.run((e, scan) => {
+		if (e) {
+			return console.log('ERR : ', e);
+		}
+
+		// TODO: clear
+		console.log('scan', scan);
+	});
+});
+
+//# sourceMappingURL=index-compiled.js.map
